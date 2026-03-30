baab95f0e1547c8e99bc9bd01d68ec87d6c7b972
anna-l-1114 <leanna1111555@gmail.com>
2026-03-30 10:12:44 -0400

    Add files via upload

---
+++ LargeCat.java
@@ -0,0 +1,14 @@
+// Implemented by Anna
+public class LargeCat extends Cat {
+
+    public LargeCat(String name, int age) {
+        super(name, age);
+    }
+
+    // prints large meow
+    @Override
+    public void makeSound() {
+        System.out.println("MEOW!");
+    }
+    
+}
