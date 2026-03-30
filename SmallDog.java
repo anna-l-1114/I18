6f7b6d82157bf81c5c1547489396d4fbb48319e9
KINGYSB <44515034+KINGYSB@users.noreply.github.com>
2026-03-30 10:24:04 -0400

    Create SmallDog class extending Dog

---
+++ SmallDog.java
@@ -0,0 +1,14 @@
+// Implemented by: Youssef Bey
+public class SmallDog extends Dog {
+
+    // constructor
+    public SmallDog(String name, int age) {
+        super(name, age);
+    }
+
+    // prints small dog sound
+    @Override
+    public void makeSound() {
+        System.out.println("woof woof");
+    }
+}
