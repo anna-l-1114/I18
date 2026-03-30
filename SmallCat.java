a8819357a5fd160a7cd0b27211dbeb28aab3a629
Colby-Watt <cbw65@pitt.edu>
2026-03-30 10:18:05 -0400

    Update SmallCat.java
    added comments

--- SmallCat.java
+++ SmallCat.java
@@ -1,9 +1,12 @@
+//Implemented by Colby
 public Class SmallCat extends Cat {
 	
+	//Uses superclass constructor
 	public SmallCat(String name, int age) {
 		super(name, age)
 	}
 	
+	//Override
 	public makeSound() {
 		System.out.println(this.getName() + " says meow...");
 	}
