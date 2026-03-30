127c12a0994651ad85cb6c82b08c1a354d84c772
KINGYSB <44515034+KINGYSB@users.noreply.github.com>
2026-03-30 10:21:23 -0400

    Add getters for name and age in Animal class
    Added getter methods for name and age in Animal class.

--- Animal.java
+++ Animal.java
@@ -1,3 +1,4 @@
+// Implemented by: Youssef Bey (Animal.java)
 public abstract class Animal{
     private String name;
     private int age;
@@ -6,6 +7,14 @@ public abstract class Animal{
     public Animal(String name, int age){
         this.name = name;
         this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
     }
 
     public abstract void makeSound();
