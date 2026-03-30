5d275d13e42222d34714e1e90542b6dee0dece8f
KINGYSB <44515034+KINGYSB@users.noreply.github.com>
2026-03-30 10:21:51 -0400

    Extend App to include more animal classes
    Added LargeCat and SmallCat classes, along with SmallDog to the main method.

--- App.java
+++ App.java
@@ -1,9 +1,16 @@
+// Implemented by: Youssef Bey (App.java)
 public class App {
     public static void main(String[] args) throws Exception {
         Cat cat = new Cat("Whiskers", 3);
         Dog dog = new Dog("Buddy", 5);
+        LargeCat largeCat = new LargeCat("Leo", 6);
+        SmallCat smallCat = new SmallCat("Mimi", 2);
+        SmallDog smallDog = new SmallDog("Rocky", 1);
 
         cat.makeSound();
         dog.makeSound();
+        largeCat.makeSound();
+        smallCat.makeSound();
+        smallDog.makeSound();
     }
 }
